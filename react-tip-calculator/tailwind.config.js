eaeafdb33080c6109ba7cd7e2b88cf76b4a5e665
Ivan Leon <leonsanp@gmail.com>
2023-09-28 22:59:48 -0500

    Avance react tip

---
+++ react-tip-calculator/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#00494d",
+      }
+    },
+  },
+  plugins: [],
+}
+
