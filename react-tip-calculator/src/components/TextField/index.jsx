0246dd33d0630d37ac56396d7f28f984b307d101
Ivan Leon <leonsanp@gmail.com>
2023-09-29 22:58:27 -0500

    Parte 2 tip calculator react

--- react-tip-calculator/src/components/TextField/index.jsx
+++ react-tip-calculator/src/components/TextField/index.jsx
@@ -1,21 +1,34 @@
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
 import dollarIcon from "../../assets/icon-dollar.svg";
 import personIcon from "../../assets/icon-person.svg";
 
-export default function TextField({ icon, name, placeholder }){
-    const icons = {
-        dollar: dollarIcon,
-        person: personIcon,
-    };
+export default function TextField({ icon, name, placeholder, value, onChange }) {
+  const inputRef = useRef(null);
 
-    return(
-        <div className="flex items-center p-3 rounded-md bg-green-100/50 cursor-text">
-          <img src={icons[icon]} alt="" />
-          <input
-            type="text"
-            name={name}
-            placeholder={placeholder}
-            className="w-full text-right outline-none bg-transparent text-green-900 text-xl font-semibold"
-          />
-        </div>
-    );
+  const icons = {
+    dollar: dollarIcon,
+    person: personIcon,
+  };
+
+  const handleInputRef = () => inputRef.current.focus();
+
+  return (
+    <div
+      onClick={handleInputRef}
+      className="flex items-center p-3 rounded-md bg-green-100/50 cursor-text"
+    >
+      <img src={icons[icon]} alt="" />
+      <input
+        ref={inputRef}
+        type="text"
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        type="number"
+        className="w-full text-right outline-none bg-transparent text-green-900 text-xl font-semibold"
+      />
+    </div>
+  );
 }
